a6b272c1ebec782d913eb2af1b5a97e419b954b9
Liz Verde <>
2017-12-26 20:15:04 +0000

    Adding a method and onChange method to the button and submit so that it can read the state of the input and replace it in the initial state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			deadline: 'January 7, 2018'
+			deadline: 'January 7, 2018', //set up the initial state
+			newDeadline: '' //set up another key with an empty string
 		} //these can be a string, number, or another object etc //calling this.state.deadline will take the this that we've called and we can place in any date we like
 	}
 
@@ -19,14 +20,15 @@ class App extends Component {
 
 	//we need to add an anonymous function allows us to call it just once, so that it doesn't loop and crash the app
 	changeDeadline() {
-		this.setState({ deadline: 'November 25, 2017' })
+		console.log('state', this.state);
+		this.setState({ deadline: this.state.newDeadline });
 	}
 
 
 	render(){
 		return (
 		<div className="App">
-			<div className="App-title">Countdown to { this.state.deadline }</div>
+			<div className="App-title">Countdown to { this.state.deadline }</div> {/*this is calling the initial state above */}
 			<div>
 				<div className="Clock-days" >14 days</div>
 				<div className="Clock-hours">30 hours</div>
@@ -35,7 +37,12 @@ class App extends Component {
 			</div>
 
 			<div>
-				<input placeholder='new date'/>
+				{/*the input is asking for a new date*/}
+				{/*We want to collect the input of the new date*/}
+				{/*Then call the changeDeadline() method on the submit button */}
+				<input 
+					placeholder='new date' 
+					onChange={ event => this.setState({ newDeadline: event.target.value }) }/> 
 				<button onClick={ () => this.changeDeadline() }>Submit</button> 
 			</div>
 		</div>
