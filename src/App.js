2e335e92b1afc18129044e858ea70ce10fdf0c66
Liz Verde <>
2017-12-26 21:08:51 +0000

    Finishing the app with local state, adding the life cycle and helper methods and some styling

--- src/App.js
+++ src/App.js
@@ -1,55 +1,43 @@
 import React, { Component } from 'react';
-import './App.css'
+import Clock from './components/Clock';
+import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
-//classes are made for their object orientated capability
 class App extends Component {
-	//the constructor allows us to have local state within our class
-	//props refers to inherited data from a component
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'January 8, 2018',
+      newDeadline: ''
+    }
+  }
 
-		this.state = {
-			deadline: 'January 7, 2018', //set up the initial state
-			newDeadline: '' //set up another key with an empty string
-		} //these can be a string, number, or another object etc //calling this.state.deadline will take the this that we've called and we can place in any date we like
-	}
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
 
-	//a method that changes the state, we need to use the setState() method, never mutate or change state directly. this.setState() method helps us create a new object
-
-	//we then add this method to the button on the onClick
-
-	//we need to add an anonymous function allows us to call it just once, so that it doesn't loop and crash the app
-	changeDeadline() {
-		console.log('state', this.state);
-		this.setState({ deadline: this.state.newDeadline });
-	}
-
-
-	render(){
-		return (
-		<div className="App">
-			<div className="App-title">Countdown to { this.state.deadline }</div> {/*this is calling the initial state above */}
-			<div>
-				<div className="Clock-days" >14 days</div>
-				<div className="Clock-hours">30 hours</div>
-				<div className="Clock-mins">15 mins</div>
-				<div className="Clock-secs">20 secs</div>
-			</div>
-
-			<div>
-				{/*the input is asking for a new date*/}
-				{/*We want to collect the input of the new date*/}
-				{/*Then call the changeDeadline() method on the submit button */}
-				<input 
-					placeholder='new date' 
-					onChange={ event => this.setState({ newDeadline: event.target.value }) }/> 
-				<button onClick={ () => this.changeDeadline() }>Submit</button> 
-			</div>
-		</div>
-		)
-	}
-
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">
+          Countdown to {this.state.deadline}
+        </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
+        <Form inline>
+          <FormControl
+            className="Deadline-input"
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadline()}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
 }
 
-//it's it's own component and can now be reused
 export default App;
